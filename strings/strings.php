9e9bebb7b10de5dc6789396da8d75c5228c98b49
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-01-26 21:53:43 +0100

    add Strings

---
+++ strings/strings.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// the length of the string
+echo strlen("Hello world!"); // outputs 12
+
+echo '<br>';
+
+// the number of words
+echo str_word_count("Hello world!"); // outputs 2
+
+echo '<br>';
+
+// Replace the text "world" with "Dolly"
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+
+
+?>
