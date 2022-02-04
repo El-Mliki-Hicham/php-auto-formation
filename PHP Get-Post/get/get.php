0a94569f102e70854771f16e4d0abdd848d81093
hicham <ELmliki.hicham@gmail.com>
2022-02-04 02:30:42 -0800

    get

--- PHP Get-Post/get/get.php
+++ PHP Get-Post/get/get.php
@@ -1,8 +1,8 @@
 <?php
 
         
-        echo "Welcome " . $_GET['name'] . "<br />";
-        echo "You are " . $_GET['age'] . " years old.";
+        echo   $_GET['name'] . "<br />";
+        echo   $_GET['age'] . " years old.";
  
 
 ?>
