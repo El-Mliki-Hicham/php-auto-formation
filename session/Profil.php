9a902206801b25881cc83d930bc5de142c980c28
hicham <ELmliki.hicham@gmail.com>
2022-02-18 02:58:45 -0800

    add

--- session/Profil.php
+++ session/Profil.php
@@ -5,8 +5,8 @@ $age = $_POST["age"];
 
 
 if(isset($_POST['btn'])){
-    echo  " your Name is  : ".$_SESSION['name'] = $name ; 
-    echo  "<br>"."your age is : ". $_SESSION["age"]=$age;
+    echo  " your Name is  : ".$_SESSION['Name'] = $name ; 
+    echo  "<br>"."your age is : ". $_SESSION["Age"]=$age;
     
 }
 
