314d5c0287508427a82236af40448e9399c6808b
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-08 20:43:07 +0100

    switch case

---
+++ Switch Statement/exemple.php
@@ -0,0 +1,17 @@
+<?php
+$color = "red";
+
+switch ($color) {
+    case 'red':
+        echo "color is red";
+        break;
+    case 'blue':
+        echo "color is blue";
+        break;
+    
+    default:
+        echo"non color";
+    break;
+}
+
+?>
