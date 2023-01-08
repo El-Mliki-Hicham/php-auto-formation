15d8e2e897ea4f2b15e263e8db3e3bd0d6fba6b8
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-08 20:36:33 +0100

    constant

---
+++ Constants/exemple.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+//define string
+define("GREETING", "Welcome to W3Schools.com!");
+echo GREETING;
+
+// define array
+define("cars", [
+  "Alfa Romeo",
+  "BMW",
+  "Toyota"
+]);
+echo cars[0];
+?> 
+
+</body>
+</html>
