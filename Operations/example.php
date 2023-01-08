a70b20026237adffa9355a6bf5f2b140aff210f3
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-08 20:39:22 +0100

    operations

---
+++ Operations/example.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+echo "<br>";
+echo $x - $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x * $y;
+?>  
+
+</body>
+</html>
