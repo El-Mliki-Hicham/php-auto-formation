a050862b6e8f759fa184185367b1b30e81be6eab
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-18 23:31:40 +0100

    code

--- Object Oriented/polymorphisme/exe.php
+++ Object Oriented/polymorphisme/exe.php
@@ -12,17 +12,14 @@ abstract class Person {
     
      public function Ajouter($data);
      public function afficherData();
+     
  
 }
  class IGestion implements Gestion  {
-    // use Person;
-    public  $listData = array();
+    private  $listData = array();
      public function Ajouter($data)
     {
        $_data= implode(" ", $data);
-    // $_data = (object)$data;
-    //    var_dump($_data);
-    //      die();
         array_push($this->listData,$_data);
     }
     public function afficherData(){
@@ -30,33 +27,23 @@ abstract class Person {
         foreach($data as $value){
             echo $value .'<br>';
         }
-    //     var_dump($data);
-        
-    // // var_dump(implode(",", $data));
-    // die();
-
     }
 
 
 }
 class Formateur extends Person {
-   
    public function addItems($Nom ,$Prenom){
     $this->Nom = $Nom;
     $this->Prenom = $Prenom;
-    return [$Nom,$Prenom];
+    return [$Nom,$Prenom,"is a Formateur"];
+    }  
     
-    
-}  
-
 }
 class Stagiaire  extends Person {
     public function addItems($Nom, $Prenom ){
         $this->Nom = $Nom;
         $this->Prenom = $Prenom;
-        return [$Nom,$Prenom];
-        
-        
+        return [$Nom,$Prenom,"is a stagiaire"];    
     }  
 }
 
@@ -65,10 +52,10 @@ $Stagiaire = new Stagiaire;
 $Gestion = new IGestion;
 $AddFormateur =  $formateur->addItems('hicham',"mliki");
 $StagiaireAdd =  $Stagiaire->addItems('nada',"stitou");
-$StagiaireAdde =  $Stagiaire->addItems('nadaz',"szztitou");
+$StagiaireAdd2 =  $Stagiaire->addItems('nadaz',"szztitou");
 $Gestion->Ajouter($AddFormateur);
 $Gestion->Ajouter($StagiaireAdd);
-$Gestion->Ajouter($StagiaireAdde);
+$Gestion->Ajouter($StagiaireAdd2);
 $Gestion->afficherData();
 
 
